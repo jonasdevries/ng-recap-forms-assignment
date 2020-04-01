1e04b8cf234347522a7dbc1aacaff95b8a20f97a
Jonas Devries <jonas@good-it.be>
2020-04-01 11:27:41 +0200

    exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('subscriptionForm') subscriptionForm: NgForm;
+
+  subscriptionData = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+
+  subscriptionFormSubmitted = false;
+  subscription = 'Advanced';
+
+  onSubmit(subscriptionForm: NgForm) {
+    this.subscriptionFormSubmitted = true;
+    console.log(subscriptionForm);
+
+    console.log(this.subscriptionForm.value.email);
+    console.log(this.subscriptionForm.value.subscription);
+    console.log(this.subscriptionForm.value.password);
+
+    this.subscriptionData.email = this.subscriptionForm.value.email;
+    this.subscriptionData.subscription = this.subscriptionForm.value.subscription;
+    this.subscriptionData.password = this.subscriptionForm.value.password;
+  }
 
 }
